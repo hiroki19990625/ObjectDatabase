5f9ecbdd4ef7e6fde8889cd9d82939f5dab5f0a9
hiroki19990625 <hiroki19990625@gmail.com>
2019-09-26 00:40:55 +0900

    テストを変更

--- ObjectDatabase.Tests/Tests.cs
+++ ObjectDatabase.Tests/Tests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using NUnit.Framework;
 using ObjectDatabase.Tests.Models;
 
@@ -37,16 +39,28 @@ namespace ObjectDatabase.Tests
         {
             ObjectDatabase database = new ObjectDatabase("ObjectDatabase.accdb");
             DataTable<TestDataModel> table = new DataTable<TestDataModel>("Test");
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
             database.AddTable(table);
+            stopwatch.Stop();
+            Console.WriteLine($"Fetch {stopwatch.ElapsedMilliseconds}ms");
 
-            table.Delete(model => { return model.Name == "Alice"; });
+            stopwatch = Stopwatch.StartNew();
+            table.Delete(model => model.Name == "Alice");
+            stopwatch.Stop();
+            Console.WriteLine($"Delete {stopwatch.ElapsedMilliseconds}ms");
 
+            stopwatch = Stopwatch.StartNew();
             table.Insert(new TestDataModel
             {
                 Name = "Alice",
                 Age = 20,
                 Location = "US"
             });
+            stopwatch.Stop();
+            Console.WriteLine($"Insert {stopwatch.ElapsedMilliseconds}ms");
+
+            Assert.True(table.Where(model => true).First().Name == "Alice");
         }
     }
 }
