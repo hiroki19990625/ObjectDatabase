6fa6d1d08d5d35ec88378ecf9e310c50514c7285
hiroki19990625 <hiroki19990625@gmail.com>
2019-12-11 01:13:04 +0900

    テーブルのデータモデルを自動生成

--- ObjectDatabase/ObjectDatabase.cs
+++ ObjectDatabase/ObjectDatabase.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.OleDb;
+using System.IO;
 using LogAdapter;
 
 namespace ObjectDatabase
@@ -21,6 +25,7 @@ namespace ObjectDatabase
         /// </summary>
         /// <param name="file">Accessファイル</param>
         /// <param name="version">プロバイダーのバージョン</param>
+        /// <param name="logCallback">ログのコールバック</param>
         public ObjectDatabase(string file, string version = "12.0", Action<ILogMessage> logCallback = null)
         {
             if (logCallback != null)
@@ -71,14 +76,85 @@ namespace ObjectDatabase
             return _tables[name];
         }
 
+        /// <summary>
+        /// ログのコールバックを追加します。
+        /// </summary>
+        /// <param name="callback">ログのコールバック</param>
         public void AddLogEvent(Action<ILogMessage> callback)
         {
             _logger.AddCallback(callback);
         }
 
+        /// <summary>
+        /// ログのコールバックを削除します。
+        /// </summary>
+        /// <param name="callback"></param>
         public void RemoveLogEvent(Action<ILogMessage> callback)
         {
             _logger.RemoveCallback(callback);
+        }
+
+        /// <summary>
+        /// データベースからテーブルを自動生成します。
+        /// </summary>
+        public void GenerateCode(string projectName)
+        {
+            CodeCompileUnit compileUnit = new CodeCompileUnit();
+            CodeNamespace importCodeNamespace = new CodeNamespace();
+            importCodeNamespace.Imports.Add(new CodeNamespaceImport("ObjectDatabase"));
+            compileUnit.Namespaces.Add(importCodeNamespace);
+            CodeNamespace codeNamespace = new CodeNamespace($"{projectName}.DataModels");
+            compileUnit.Namespaces.Add(codeNamespace);
+
+            var tables = _connection.GetSchema("Tables");
+            foreach (DataRow row in tables.Rows)
+            {
+                var name = row["TABLE_NAME"].ToString();
+                CodeTypeDeclaration typeDeclaration = new CodeTypeDeclaration(name);
+                typeDeclaration.BaseTypes.Add(new CodeTypeReference(typeof(DataModel)));
+                var columns = _connection.GetSchema("Columns").Select($"TABLE_NAME='{name}'");
+                foreach (var column in columns)
+                {
+                    string colName = column["COLUMN_NAME"].ToString();
+                    OleDbType type = (OleDbType) column["DATA_TYPE"];
+                    var rows = _connection.GetSchema("Indexes")
+                        .Select($"TABLE_NAME='{name}' AND COLUMN_NAME='{colName}'");
+                    bool isKey = rows.Length > 0 && bool.Parse(rows[0]["UNIQUE"].ToString());
+                    CodeMemberField field = new CodeMemberField(type.CreateType(), colName.ToLower());
+                    CodeMemberProperty property = new CodeMemberProperty
+                    {
+                        Type = new CodeTypeReference(type.CreateType()),
+                        Name = colName,
+                        HasGet = true,
+                        HasSet = true,
+                        Attributes = MemberAttributes.Public | MemberAttributes.Final
+                    };
+                    property.CustomAttributes.Add(new CodeAttributeDeclaration(
+                        new CodeTypeReference(typeof(SerializePropertyAttribute)),
+                        new CodeAttributeArgument(new CodePrimitiveExpression(colName)),
+                        new CodeAttributeArgument("IsKey", new CodePrimitiveExpression(isKey)),
+                        new CodeAttributeArgument("RelationKey", new CodePrimitiveExpression(isKey))));
+                    property.GetStatements.Add(
+                        new CodeMethodReturnStatement(new CodeFieldReferenceExpression(null, colName.ToLower())));
+                    property.SetStatements.Add(
+                        new CodeAssignStatement(new CodeFieldReferenceExpression(null, colName.ToLower()),
+                            new CodePropertySetValueReferenceExpression()));
+                    typeDeclaration.Members.Add(field);
+                    typeDeclaration.Members.Add(property);
+                }
+
+                codeNamespace.Types.Add(typeDeclaration);
+            }
+
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CS");
+            StringWriter writer = new StringWriter();
+            provider.GenerateCodeFromCompileUnit(compileUnit, writer, new CodeGeneratorOptions
+            {
+                IndentString = "    "
+            });
+
+            Directory.CreateDirectory("GeneratedCode");
+            File.WriteAllText("GeneratedCode/DatabaseGenerationCode.cs", writer.ToString());
         }
 
         public void Dispose()
@@ -105,6 +181,89 @@ namespace ObjectDatabase
         internal static void OperationLog(this ILogger logger, string operation)
         {
             logger.Log("Operation", operation);
+        }
+
+        internal static Type CreateType(this OleDbType type)
+        {
+            switch (type)
+            {
+                case OleDbType.Empty:
+                    return typeof(void);
+                case OleDbType.SmallInt:
+                    return typeof(short);
+                case OleDbType.Integer:
+                    return typeof(int);
+                case OleDbType.Single:
+                    return typeof(float);
+                case OleDbType.Double:
+                    return typeof(double);
+                case OleDbType.Currency:
+                    return typeof(decimal);
+                case OleDbType.Date:
+                    return typeof(DateTime);
+                case OleDbType.BSTR:
+                    return typeof(string);
+                case OleDbType.IDispatch:
+                    return typeof(object);
+                case OleDbType.Error:
+                    return typeof(Exception);
+                case OleDbType.Boolean:
+                    return typeof(bool);
+                case OleDbType.Variant:
+                    return typeof(object);
+                case OleDbType.IUnknown:
+                    return typeof(object);
+                case OleDbType.Decimal:
+                    return typeof(decimal);
+                case OleDbType.TinyInt:
+                    return typeof(sbyte);
+                case OleDbType.UnsignedTinyInt:
+                    return typeof(byte);
+                case OleDbType.UnsignedSmallInt:
+                    return typeof(ushort);
+                case OleDbType.UnsignedInt:
+                    return typeof(uint);
+                case OleDbType.BigInt:
+                    return typeof(long);
+                case OleDbType.UnsignedBigInt:
+                    return typeof(ulong);
+                case OleDbType.Filetime:
+                    return typeof(DateTime);
+                case OleDbType.Guid:
+                    return typeof(Guid);
+                case OleDbType.Binary:
+                    return typeof(byte[]);
+                case OleDbType.Char:
+                    return typeof(string);
+                case OleDbType.WChar:
+                    return typeof(string);
+                case OleDbType.Numeric:
+                    return typeof(decimal);
+                case OleDbType.DBDate:
+                    return typeof(DateTime);
+                case OleDbType.DBTime:
+                    return typeof(TimeSpan);
+                case OleDbType.DBTimeStamp:
+                    return typeof(TimeSpan);
+                case OleDbType.PropVariant:
+                    return typeof(object);
+                case OleDbType.VarNumeric:
+                    return typeof(decimal);
+                case OleDbType.VarChar:
+                    return typeof(string);
+                case OleDbType.LongVarChar:
+                    return typeof(string);
+                case OleDbType.VarWChar:
+                    return typeof(string);
+                case OleDbType.LongVarWChar:
+                    return typeof(string);
+                case OleDbType.VarBinary:
+                    return typeof(byte[]);
+                case OleDbType.LongVarBinary:
+                    return typeof(byte[]);
+                default:
+                    return typeof(void);
+            }
         }
     }
 }
